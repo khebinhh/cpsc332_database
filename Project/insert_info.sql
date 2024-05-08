594e33f1e70ec6a2f60f028a267335c6747b95eb
Kevin <60121349+khebinhh@users.noreply.github.com>
2024-05-08 22:19:03 +0000

    pre-made database for requirement

---
+++ Project/insert_info.sql
@@ -0,0 +1,3 @@
+--3 Professors ex.
+INSERT INTO Professor(SSN, firstName, lastName, streetName, city, state, zip, areaCode, phoneNumber, sex, title, salary, degrees) VALUES ('423897106', 'Swayam', 'Pati', '123 Main St', 'Santa Clara', 'CA', '95050', '650', '6493785', 'F', 'Dr.', '80000', 'Ph.D in Computer Science');
+INSERT INTO Professor(SSN, firstName, lastName, streetName, city, state, zip, areaCode, phonenumber, sex, title, salary, degrees) VALUES();
