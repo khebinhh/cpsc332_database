0f9b48f0356a82b82df7b16b20a1eb6ed398ddad
Kevin <60121349+khebinhh@users.noreply.github.com>
2024-05-08 21:19:27 +0000

    completed w/ 1-6

--- Project/DDL.sql
+++ Project/DDL.sql
@@ -1,15 +1,89 @@
 -- This is used to create the tablenames and the columns in the database
+--1)Professor
 CREATE TABLE Professor(
   SSN VARCHAR(9) PRIMARY KEY,
-  Name VARCHAR(20),            --testing size
-  StreetName VARCHAR(20),      --testing size
-  City VARCHAR (10),           --testing size
-  State VARCHAR(2),            --two for just the two letter abbreviation
-  Zip VARCHAR(5)
-  AreaCode VARCHAR(3),         --telephone section
-  PhoneNumber BIGINT,          --telephone section
-  Sex CHAR(1),                 -- abbreviation for m or f
-  Tite VARCHAR(15),            --testing size
-  Salary DECIMAL(10,2), 
-  Degrees VARCHAR(255), --testing huge size in case of mult. degree
+  firstName VARCHAR(20),            
+  lastName VARCHAR(20),
+  streetName VARCHAR(20),      --testing size
+  city VARCHAR (10),           --testing size
+  state VARCHAR(2),            --two for just the two letter abbreviation
+  zip VARCHAR(5)
+  areaCode VARCHAR(3),         --telephone section
+  phoneNumber BIGINT,          --telephone section
+  sex CHAR(1),                 -- abbreviation for m or f
+  title VARCHAR(15),            --testing size
+  salary DECIMAL(10,2), --10 for the number of digits, 2 for the number of decimals
+  degrees VARCHAR(255), --testing huge size in case of mult. degree
+);
+--2)Departments
+CREATE TABLE Department(
+  deptID INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(20),
+  telephone BIGINT,
+  officeLocation VARCHAR(255),
+  chairpersonSSN VARCHAR(11),
+  FOREIGN KEY (chairpersonSSN) REFERENCES Professor(SSN)
+);
+--3a)Courses
+CREATE TABLE Courses(
+  courseID INT PRIMARY KEY,
+  title VARCHAR(20),
+  textbook VARCHAR(20),
+  units INT,
+  deptID INT,
+  FOREIGN KEY (deptID) REFERENCES Department(deptID)
+);
+--3b)Prerequisites
+CREATE TABLE Prerequisites(
+  courseID INT,
+  prereqID INT,
+  PRIMARY KEY (courseID, prereqID)
+  FOREIGN KEY (courseID) REFERENCES Courses(courseID),
+  FOREIGN KEY (prereqID) REFERENCES Courses(courseID)
+);
+--4)Sections
+CREATE TABLE Sections(
+  courseID INT,
+  secID INT,
+  classroom VARCHAR(20),
+  seats INT,
+  meetingDays VARCHAR(20),
+  startTime TIME,
+  endTime TIME,
+  professorSSN VARCHAR(11),
+  PRIMARY KEY (courseID, secID),
+  FOREIGN KEY (courseID) REFERENCES Courses(courseID)
+  FOREIGN KEY (professorSSN) REFERENCES Professor(SSN)
+);
+--5a)Students(major)
+CREATE TABLE Student(
+  cwID VARCHAR(9) PRIMARY KEY,
+  firstName VARCHAR(20),
+  lastName VARCHAR(20),
+  streetName VARCHAR(20),
+  city VARCHAR (10),           --testing size
+  state VARCHAR(2),            --two for the abbreviation
+  zip VARCHAR(5)
+  areaCode VARCHAR(3),         --telephone section
+  phoneNumber BIGINT,          --telephone section
+  majordeptID INT,
+  FOREIGN KEY (majordeptID) REFERENCES Department(deptID)
+);
+--5b)Student(minor)
+CREATE TABLE sMinor(
+  cwID VARCHAR(9),
+  minordeptID INT,
+  PRIMARY KEY (cwID, minordeptID),
+  FOREIGN KEY (cwID) REFERENCES Student(cwID),
+  FOREIGN KEY (minordeptID) REFERENCES Department(deptID)
+);
+--6)Enrollments
+CREATE TABLE Enrollments(
+  cwID VARCHAR(9),
+  courseID INT,
+  secID INT,
+  grade VARCHAR(2),
+  PRIMARY KEY (cwID, courseID, secID),
+  FOREIGN KEY (cwID) REFERENCES Student(cwID),
+  FOREIGN KEY (courseID, secID) REFERENCES Sections(courseID, secID)
 );
